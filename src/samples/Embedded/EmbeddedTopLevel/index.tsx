bb577826beb59a4ec7fce546661648557742b00a
Ganesh <fastertabla@gmail.com>
2024-11-20 17:22:34 +0000

    tst

--- src/samples/Embedded/EmbeddedTopLevel/index.tsx
+++ src/samples/Embedded/EmbeddedTopLevel/index.tsx
@@ -448,7 +448,6 @@ export default function EmbeddedTopLevel() {
         <img src='./assets/img/aiprates4.jpg' />
         <img src='./assets/img/aiprates6.jpg' />
         <div className={classes.embedShoppingOptions}>{theOptions}</div>
-        <img src='./assets/img/aiprates7.jpg' />
       </div>
     );
   }
@@ -461,7 +460,7 @@ export default function EmbeddedTopLevel() {
             <b>Thank you!</b>
             <br />
             <br />
-            Your Agreement in Principle is under review with the case reference AIP-6010. Representative from Lloyds bank will contact you with in the
+            Your Agreement in Principle is under review with the case reference AIP-6024. Representative from Lloyds bank will contact you with in the
             next couple of days to discuss next steps. <br />
             <br />
             If you have any questions, you can contact us directly at <b>08005551234</b> or you can chat with us.
